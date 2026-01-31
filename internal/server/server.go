254ad824f6568fe1c156e9e9f70e4e7f279d3688
Lee Martin <hello@leejohnmartin.co.uk>
2026-01-31 16:53:00 +0000

    feat: Simple HTML templater

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/leemartin77/handicap/internal/config"
@@ -30,9 +31,15 @@ func NewServer(ctx context.Context, cfg *config.Config) (Server, error) {
 func (s *serverState) RunServer() error {
 	// TODO: make setup part of initialisation
 	router := gin.Default()
+	router.LoadHTMLGlob("web/template/*")
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": s.strg.GetTestData(c),
+		})
+	})
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"Message": s.strg.GetTestData(ctx),
 		})
 	})
 	return router.Run()
